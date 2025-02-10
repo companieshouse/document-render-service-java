7db69ffb481cda2dd37cf3842044f2456cb88af8
Steve Roberts <sroberts2@companieshouse.gov.uk>
2025-02-10 14:31:33 +0000

    Initial commit

---
+++ src/main/java/uk/gov/companieshouse/documentrenderservicejava/DocumentRenderServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.documentrenderservicejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentRenderServiceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentRenderServiceJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/documentrenderservicejava/DocumentRenderServiceJavaApplicationTests.java
@@ -0,0 +1,14 @@
+package uk.gov.companieshouse.documentrenderservicejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentRenderServiceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//test context load
+	}
+
+}

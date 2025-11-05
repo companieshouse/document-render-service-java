f595465036dda7c07771f49c92a09747b649b538
Mike Kelly <mkelly1@companieshouse.gov.uk>
2025-11-05 15:39:29 +0000

    Update service version variable name to match pipeline.

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -63,7 +63,7 @@ module "ecs-service" {
   # Docker container details
   docker_registry   = var.docker_registry
   docker_repo       = local.docker_repo
-  container_version = var.document_render_service_version
+  container_version = var.document_render_service_java_version
   container_port    = local.container_port
 
   # Service configuration

ffe417ee2504d7191f0450ade6f02cfe5e54c0d3
jbishop2-ch <jbishop2@companieshouse.gov.uk>
2025-11-13 16:52:43 +0000

    Code changes to address Sonar PR issues

--- src/main/java/uk/gov/companieshouse/documentrender/parser/S3LocationParser.java
+++ src/main/java/uk/gov/companieshouse/documentrender/parser/S3LocationParser.java
@@ -20,10 +20,9 @@ import uk.gov.companieshouse.logging.Logger;
 public class S3LocationParser {
 
     private static final Pattern LOCATION_REGEX = Pattern.compile("^s3://([a-zA-Z0-9\\-_.]+)/(.*)$");
-    //private static final Pattern LOCATION_WITH_FILENAME_REGEX = Pattern.compile("^s3://([a-zA-Z0-9\\-_.]+)/(.+)/(.+\\..+)$");
 
     /**
-     * We have a number of Sonar PR issues around this regular expression, but they're mitigated elsewhere in the code.
+     * We have a multiple Sonar PR issues around this regular expression, but they're mitigated elsewhere in the code.
      * The length of the filename is checked elsewhere, and the bucket name and path are controlled inputs.
      */
     @SuppressWarnings({"java:S5852","java:S5998"})
